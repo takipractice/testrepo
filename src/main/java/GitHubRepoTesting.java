ccb5811ffe6bcc4e9c70c059b169756f2ab7fa79
takipractice <takacs.imre5520@gmail.com>
2022-10-24 14:44:52 +0200

    test3

--- src/main/java/GitHubRepoTesting.java
+++ src/main/java/GitHubRepoTesting.java
@@ -1,6 +1,14 @@
 public class GitHubRepoTesting {
 
     public static void main(String[] args) {
-        System.out.println("Testing GitHub");
+        int a = 4;
+        int b = 8;
+        System.out.println("a = " + a + " b = " + b);
+
+        int c = a;
+        a = b;
+        b = c;
+        System.out.println("a = " + a + " b = " + b);
+
     }
 }
